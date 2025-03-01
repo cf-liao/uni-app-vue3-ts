6774066023adedb1e042c03d7d7974f58546cf65
cf-liao <1781071763@qq.com>
2025-03-02 01:10:10 +0800

    feat:小程序端 pinia 持久化

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createSSRApp } from 'vue'
+// 导入 pinia 实例
 import pinia from './stores'
 
 import App from './App.vue'
 export function createApp() {
+  // 创建 vue 实例
   const app = createSSRApp(App)
 
+  // 使用 pinia
   app.use(pinia)
   return {
     app,
