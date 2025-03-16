1bf83fd8c308da924a6f80f95d9829dfe388350c
cf-liao <1781071763@qq.com>
2025-03-16 23:45:59 +0800

    feat:猜你喜欢组件封装

--- src/components/components.d.ts
+++ src/components/components.d.ts
@@ -1,6 +1,9 @@
 import XtxSwiper from './XtxSwiper.vue'
+import XtxGuess from './XtxGuess.vue'
+
 declare module 'vue' {
   export interface GlobalComponents {
     XtxSwiper: typeof XtxSwiper
+    XtxGuess: typeof XtxGuess
   }
 }
