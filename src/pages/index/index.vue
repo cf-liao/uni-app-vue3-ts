895d6ca645785b7083b61a3b9d8f131e1a422397
cf-liao <1781071763@qq.com>
2025-03-17 01:02:59 +0800

    feat:猜你喜欢分页准备

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -6,6 +6,7 @@ import CategoryPanel from './components/CategoryPanel.vue'
 import { onLoad } from '@dcloudio/uni-app'
 import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
 import HotPanel from './components/HotPanel.vue'
+import type { XtxGuessInstance } from '@/types/component'
 
 // 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
@@ -35,19 +36,26 @@ onLoad(() => {
   getHomeCategoryData()
   getHomeHotData()
 })
+
+// 获取猜你喜欢组件实例
+const guessRef = ref<XtxGuessInstance>()
+// 滚动触底
+const onScrolltolower = () => {
+  guessRef.value?.getMore()
+}
 </script>
 
 <template>
   <!-- 自定义导航栏 -->
   <CustomNavbar />
-  <scroll-view scroll-y>
+  <scroll-view @scrolltolower="onScrolltolower" scroll-y>
     <!-- 自定义轮播图 -->
     <XtxSwiper :list="bannerList" />
     <!-- 分类面板 -->
     <CategoryPanel :list="categoryList" />
     <!-- 热门推荐 -->
     <HotPanel :list="hotList" />
-    <XtxGuess />
+    <XtxGuess ref="guessRef" />
   </scroll-view>
 </template>
 
--- src/types/component.d.ts
+++ src/types/component.d.ts
@@ -3,9 +3,15 @@
  *   现调整为
  * declare module 'vue'
  */
-import 'vue'
+import type XtxGuess from '@/components/XtxGuess.vue'
+import XtxSwiper from './XtxSwiper.vue'
+
 declare module 'vue' {
   export interface GlobalComponents {
-    //
+    XtxSwiper: typeof XtxSwiper
+    XtxGuess: typeof XtxGuess
   }
 }
+
+// 组件实例类型
+export type XtxGuessInstance = InstanceType<typeof XtxGuess>
