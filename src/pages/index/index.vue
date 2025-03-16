d5db80ce81177b34c2a3829a186a311610fa5b59
cf-liao <1781071763@qq.com>
2025-03-16 17:45:55 +0800

    feat:前台分类数据类型定义和渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,7 +4,7 @@ import { ref } from 'vue'
 import CustomNavbar from './components/CustomNavbar.vue'
 import CategoryPanel from './components/CategoryPanel.vue'
 import { onLoad } from '@dcloudio/uni-app'
-import type { BannerItem } from '@/types/home'
+import type { BannerItem, CategoryItem } from '@/types/home'
 
 // 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
@@ -15,8 +15,10 @@ const getHomeBannerData = async () => {
 }
 
 // 获取前台分类数据
+const categoryList = ref<CategoryItem[]>([])
 const getHomeCategoryData = async () => {
   const res = await getHomeCategoryAPI()
+  categoryList.value = res.result
 }
 // 页面加载
 onLoad(() => {
@@ -31,7 +33,7 @@ onLoad(() => {
   <!-- 自定义轮播图 -->
   <XtxSwiper :list="bannerList" />
   <!-- 分类面板 -->
-  <CategoryPanel />
+  <CategoryPanel :list="categoryList" />
   <view class="index">index</view>
 </template>
 
