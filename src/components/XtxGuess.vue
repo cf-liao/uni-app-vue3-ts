5472c5381c7984d64cb4740d672a19cc2bd5cce1
cf-liao <1781071763@qq.com>
2025-03-18 00:07:19 +0800

    feat:猜你喜欢分页条件

--- src/components/XtxGuess.vue
+++ src/components/XtxGuess.vue
@@ -1,14 +1,38 @@
 <script setup lang="ts">
 import { getHomeGoodsGuessLikeAPI } from '@/services/home'
+import type { PageParams } from '@/types/global'
 import type { GuessItem } from '@/types/home'
 import { onMounted, ref } from 'vue'
 
+// 分页参数
+const pageParams: Required<PageParams> = {
+  page: 30,
+  pageSize: 10,
+}
 // 猜你喜欢的列表
 const guessList = ref<GuessItem[]>([])
+// 已结束标记
+const finish = ref(false)
 // 获取猜你喜欢列表数据
 const getHomeGoodsGuessLikeData = async () => {
-  const res = await getHomeGoodsGuessLikeAPI()
-  guessList.value = res.result.items
+  // 退出判断
+  if (finish.value === true) {
+    return uni.showToast({
+      title: '没有更多数据~',
+      icon: 'none',
+    })
+  }
+  const res = await getHomeGoodsGuessLikeAPI(pageParams)
+  // guessList.value = res.result.items
+  // 数组追加
+  guessList.value.push(...res.result.items)
+  // 分页条件
+  if (pageParams.page < res.result.pages) {
+    // 页码累加
+    pageParams.page++
+  } else {
+    finish.value = true
+  }
 }
 
 // 组件挂载完毕
@@ -41,7 +65,7 @@ defineExpose({
       </view>
     </navigator>
   </view>
-  <view class="loading-text"> 正在加载... </view>
+  <view class="loading-text"> {{ finish ? '没有更多数据~' : '正在加载...' }} </view>
 </template>
 
 <style lang="scss">
