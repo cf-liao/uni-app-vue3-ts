aebdbd1e9357943583a3c00e3447848b18a13b83
cf-liao <1781071763@qq.com>
2025-03-24 23:50:40 +0800

    feat：商品分类一级分类渲染和Tap交互

--- src/services/home.ts
+++ src/services/home.ts
@@ -2,11 +2,10 @@ import type { PageParams, PageResult } from '@/types/global'
 import type { BannerItem, CategoryItem, GuessItem, HotItem } from '@/types/home'
 import { http } from '@/utils/http'
 
-/*
-* 首页-广告区域-小程序
-* @param distributionSite 广告区域展示位置 （投放位置 投放位置 ，1为首页，2为分类商品页）默认是1
-
-*/
+/**
+ * 首页-广告区域-小程序
+ * @param distributionSite 广告区域展示位置 （投放位置 投放位置 ，1为首页，2为分类商品页）默认是1
+ */
 
 export const getHomeBannerAPI = (distributionSite = 1) => {
   return http<BannerItem[]>({
